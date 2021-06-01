b0effecc7f124f19eda080843eb912967b9148e7
Evan Huang <evanhuang117@gmail.com>
2021-06-01 00:24:14 -0700

    made some changes to styling and added spinners

--- query_service.py
+++ query_service.py
@@ -46,9 +46,10 @@ def get_miners(user_id):
 def get_miner_shares(miner_id):
     try:
         response = requests.get(API_URL + f"/miner/{miner_id}/share")
-        return response.json()
+        if response.status_code == 200 and response.json() is not None:
+            return response.json()
 
     except requests.exceptions.RequestException as e:
         print(e)
+        return None
 
-    return response
