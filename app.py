4e1e8de00fd6621949a7ad67b8b5c4e478e9469c
Evan Huang <evanhuang117@gmail.com>
2021-06-06 21:51:16 -0700

    made a more meaningful total shares bar graph

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import plotly.express as px
+import plotly.graph_objs as go
+from scipy import signal
 import pandas as pd
 import base64
 from dash.dependencies import Input, Output
@@ -104,18 +106,37 @@ def update_shares_graph(miner_id):
             dbc.Alert("One of the hamsters running our server died. Please try again.", color='danger')
         )
     frame = pd.json_normalize(share_data)
-    print(frame)
-    #multi = frame.set_index(['gpu_no', 'type']).sort_index()
 
-   # share_counts = frame.groupby(['gpu_no', 'type']).size().reset_index(name='count')
+    # get the total valid shares per time period
+    valid_sum = frame.drop(columns=['duration', 'gpu_no']) \
+        .groupby('start')['valid'] \
+        .sum() \
+        .reset_index(name='total_valid')
+    valid = go.Bar(x=frame['start'], y=frame['valid'], name='Valid shares', marker={'color': 'mediumpurple'})
+    valid_smoothed_line = go.Line(x=valid_sum['start'],
+                                  y=signal.savgol_filter(valid_sum['total_valid'], 51, 2, mode='interp'),
+                                  name='Avg valid shares',
+                                  line=dict(color="57CC99", width=2.5, shape='spline', smoothing=10))
 
-    fig = px.bar(frame, x='start', y='valid', color="gpu_no", title="Shares by GPU")
+    invalid_sum = frame.drop(columns=['duration', 'gpu_no']) \
+        .groupby('start')['valid'] \
+        .sum() \
+        .reset_index(name='total_valid')
+    invalid = go.Bar(x=frame['start'], y=frame['invalid'], name='Invalid shares', marker={'color': 'indianred'})
+    invalid_smoothed_line = go.Line(x=invalid_sum['start'],
+                                    y=signal.savgol_filter(invalid_sum['total_invalid'], 51, 2),
+                                    name='Avg invalid shares',
+                                    line=dict(color="orange", width=2.5, shape='spline', smoothing=10))
+
     return html.Div(
         dcc.Graph(
             id="shares",
-            figure=fig,
-            className="card"
-        )
+            figure={
+                'data': [valid, valid_smoothed_line, invalid, invalid_smoothed_line],
+                'layout':
+                    go.Layout(title='Valid/invalid shares over time', barmode='stack')
+            }),
+        className="card"
     )
 
 
